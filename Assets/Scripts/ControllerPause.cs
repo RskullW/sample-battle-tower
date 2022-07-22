d1c7d2a2a72fc740fd7cbcdfd42bfad0dd864d9e
Anton <mailcom.99@mail.ru>
2022-07-22 20:47:00 +0300

    feat: materials + menu end game
    Added new materials and menu end game

--- Assets/Scripts/ControllerPause.cs
+++ Assets/Scripts/ControllerPause.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
 
 public class ControllerPause : MonoBehaviour
 {
@@ -10,6 +14,8 @@ public class ControllerPause : MonoBehaviour
     public Button restartButton;
     public Button resumeButton;
     public Button shieldButton;
+    public Label message;
+    [SerializeField] private string messageTextPause;
 
     public VisualElement backgroundPauseMenu;
     // Start is called before the first frame update
@@ -21,27 +27,35 @@ public class ControllerPause : MonoBehaviour
         shieldButton = root.Q<Button>("shield");
         restartButton = root.Q<Button>("restart");
         resumeButton = root.Q<Button>("resume");
+        message = root.Q<Label>("message");
+
+        messageTextPause = "Pause";
         
         backgroundPauseMenu = root.Q<VisualElement>("pause-menu");
         
         pauseButton.clicked += PauseButtonPressed;
         restartButton.clicked += RestartButtonPressed;
         
-        backgroundPauseMenu.visible = resumeButton.visible = restartButton.visible = false;
+        backgroundPauseMenu.visible = resumeButton.visible = restartButton.visible = message.visible = false;
         pauseButton.visible = shieldButton.visible = true;
+    }
+
+    private void Update()
+    {
+        message.text = messageTextPause;
     }
 
     void ChangeVisiblePauseMenu()
     {
         if (backgroundPauseMenu.visible == false)
         {
-            backgroundPauseMenu.visible = resumeButton.visible = restartButton.visible = true;
+            backgroundPauseMenu.visible = resumeButton.visible = restartButton.visible = message.visible = true;
             pauseButton.visible = shieldButton.visible = false;
             
             return;
         }
 
-        backgroundPauseMenu.visible = resumeButton.visible = restartButton.visible = false;
+        backgroundPauseMenu.visible = resumeButton.visible = restartButton.visible = message.visible = false;
         pauseButton.visible = shieldButton.visible = true;
     }
     
